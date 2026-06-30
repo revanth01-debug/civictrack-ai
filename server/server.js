a29b604a1f1540fd9c15ecf3c3dc4e838fd1fa58
Radharapu Revanth <radharapurevanth10@gmail.com>
2026-06-30 19:22:47 +0530

    Fix IssueRoutes import and port

--- server/server.js
+++ server/server.js
@@ -3,9 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const path = require("path");
 require("dotenv").config();
-
-const issueRoutes = require("./routes/issueRoutes");
-
+const issueRoutes = require("./routes/IssueRoutes");
 const app = express();
 
 app.use(cors());
@@ -29,7 +27,7 @@ app.get("/", (req, res) => {
   res.send("CivicTrack Backend Running 🚀");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
